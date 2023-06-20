14ed3f3bcc14b3bcf9c051e0482080a5e07aad1e
fehsato <fyonezawa14@gmail.com>
2023-06-20 11:05:46 -0300

    delete

--- src/main/java/com/prova2/empresas/resources/EmpresasController.java
+++ src/main/java/com/prova2/empresas/resources/EmpresasController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +26,23 @@ public class EmpresasController {
     @Autowired
     private EmpresasService empresasService;
 
+    @GetMapping("{id}")
+    public ResponseEntity<Empresas> getEmpresa(@PathVariable int id){
+        Empresas empresa = empresasService.getEmpresaById(id);
+        return ResponseEntity.ok().body(empresa);
+    }
+
     @GetMapping
     public ResponseEntity<List<Empresas>> getEmpresas(){
         List<Empresas> empresa = empresasService.getEmpresas();
         return ResponseEntity.ok().body(empresa);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteFornecedor(@PathVariable int id){
+        empresasService.delete(id);
+        return ResponseEntity.noContent().build();
+
     }
 
     @PostMapping
--- src/main/java/com/prova2/empresas/services/EmpresasService.java
+++ src/main/java/com/prova2/empresas/services/EmpresasService.java
@@ -8,18 +8,30 @@ import org.springframework.stereotype.Service;
 import com.prova2.empresas.entities.Empresas;
 import com.prova2.empresas.repositories.EmpresasRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class EmpresasService {
     
     @Autowired
     private EmpresasRepository empresasRepository;
+
+
+    public Empresas getEmpresaById(int id){
+        return empresasRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Erro 404: Fornecedor não encontrado")
+        );
+    }
     
 
     public List<Empresas> getEmpresas(){
         return empresasRepository.findAll();
     }
     
-
+    public void delete(int id) {
+        Empresas empresa = getEmpresaById(id);
+        empresasRepository.delete(empresa);
+    }
 
     public Empresas save(Empresas empresa) {
         return empresasRepository.save(empresa);
