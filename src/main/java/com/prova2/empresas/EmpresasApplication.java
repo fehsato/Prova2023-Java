de66bae6bd439e87776e13c3dfdd7f9400a8bbc4
fehsato <fyonezawa14@gmail.com>
2023-06-20 08:26:13 -0300

    first commit

---
+++ src/main/java/com/prova2/empresas/EmpresasApplication.java
@@ -0,0 +1,13 @@
+package com.prova2.empresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresasApplication.class, args);
+	}
+
+}
